c7af8315dc3e954c18f4f283300d94c7071edb7f
Johan Stén <johan.sten@gmail.com>
2014-08-21 00:38:45 +0800

    Moved send_payment

--- stellar/__init__.py
+++ stellar/__init__.py
@@ -5,4 +5,36 @@ from ledger import *
 from transaction import *
 
 
+def send_payment(secret, account_id, destination, amount):
 
+	if not account_id:
+		seed	   = Seed.from_human(secret)
+		account_id = AccountID.from_seed(seed).to_human()
+
+	amount = _parse_amount(amount)
+
+	tx_json = {
+		'TransactionType': 	'Payment',
+		'Account': 			account_id,
+		'Amount': 			amount,
+	}
+
+	d = destination.split("?dt=")
+	if len(d) == 2:
+		tx_json['DestinationTag'] = int(d[1])
+	tx_json['Destination'] = d[0]
+
+	tx_blob = sign_transaction(secret, tx_json)
+	submit_transaction(tx_blob)
+
+
+def _parse_amount(m):
+
+	if not '.' in m:
+		m = int(m) * 1000000
+	else:
+		i, f = m.split('.')
+		m = int(i + f)
+		m *= 1000000
+		m /= 10 ** len(f)
+	return m
--- stellar_send.py
+++ stellar_send.py
@@ -4,41 +4,6 @@ import stellar
 import argparse
 
 
-def parse_amount(m):
-
-	if not '.' in m:
-		m = int(m) * 1000000
-	else:
-		i, f = m.split('.')
-		m = int(i + f)
-		m *= 1000000
-		m /= 10 ** len(f)
-	return m
-
-
-def send(secret, account_id, destination, amount):
-
-	if not account_id:
-		seed	   = stellar.Seed.from_human(secret)
-		account_id = stellar.AccountID.from_seed(seed).to_human()
-
-	amount = parse_amount(amount)
-
-	tx_json = {
-		'TransactionType': 	'Payment',
-		'Account': 			account_id,
-		'Amount': 			amount,
-	}
-
-	d = destination.split("?dt=")
-	if len(d) == 2:
-		tx_json['DestinationTag'] = int(d[1])
-	tx_json['Destination'] = d[0]
-
-	tx_blob = stellar.sign_transaction(secret, tx_json)
-	stellar.submit_transaction(tx_blob)
-
-
 def main():
 
 	parser = argparse.ArgumentParser()
@@ -56,7 +21,7 @@ def main():
 		help='Secret key')
 
 	args = parser.parse_args()
-	send(args.secret, args.source, args.destination, args.amount)
+	stellar.send_payment(args.secret, args.source, args.destination, args.amount)
 
 
 if __name__ == '__main__':
