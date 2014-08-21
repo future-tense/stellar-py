62b44841785a345acc0f7f3f820b555479345ba1
Johan Stén <johan.sten@gmail.com>
2014-08-21 11:32:18 +0800

    Added caching to federation. Federation now does lookup at the actual domains.

--- stellar/__init__.py
+++ stellar/__init__.py
@@ -5,35 +5,38 @@ from ledger import *
 from transaction import *
 
 
-def send_payment(secret, account_id, destination, amount):
+def send_payment(secret, account, destination, amount):
 
-	if not account_id:
-		seed	   = Seed.from_human(secret)
-		account_id = AccountID.from_seed(seed).to_human()
+	if not account:
+		seed	= Seed.from_human(secret)
+		account = AccountID.from_seed(seed).to_human()
+	else:
+		account = _translate_account_id(account)
 
 	amount = _parse_amount(amount)
 
 	tx_json = {
 		'TransactionType': 	'Payment',
-		'Account': 			account_id,
+		'Account': 			account,
 		'Amount': 			amount,
 	}
 
 	d = destination.split("?dt=")
 	if len(d) == 2:
 		tx_json['DestinationTag'] = int(d[1])
-	tx_json['Destination'] = d[0]
+	tx_json['Destination'] = _translate_account_id(d[0])
 
 	tx_blob = sign_transaction(secret, tx_json)
 	submit_transaction(tx_blob)
 
 
-def set_regular_key(secret, account_id, regular_key):
+
+def set_regular_key(secret, account, regular_key):
 
 	tx_json = {
 		'TransactionType':	'SetRegularKey',
-		'Account':			account_id,
-		'RegularKey':		regular_key,
+		'Account':			_translate_account_id(account),
+		'RegularKey':		_translate_account_id(regular_key),
 	}
 
 	tx_blob = sign_transaction(secret, tx_json)
@@ -50,3 +53,13 @@ def _parse_amount(m):
 		m *= 1000000
 		m /= 10 ** len(f)
 	return m
+
+
+def _translate_account_id(account):
+
+	try:
+		t = AccountID.from_human(account)
+	except Exception:
+		return get_accountid(account)
+	else:
+		return account
--- stellar/federation.py
+++ stellar/federation.py
@@ -1,14 +1,8 @@
-
-#	https://stellar.stellar.org/stellar.txt
-
-
 import urllib2
 import simplejson as json
 
-#-------------------------------------------------------------------------------
-
-FEDERATION_URL 			= "https://api.stellar.org/federation"
-REVERSE_FEDERATION_URL	= "https://api.stellar.org/reverseFederation"
+import cPickle as pickle
+import os.path
 
 #-------------------------------------------------------------------------------
 
@@ -18,11 +12,77 @@ urllib2.install_opener(opener)
 
 #-------------------------------------------------------------------------------
 
+forward_url = {}
+reverse_url = {}
+account_id  = {}
+username    = {}
 
-def get_accountid_from_username(name):
+#-------------------------------------------------------------------------------
 
-	url = FEDERATION_URL
-	query = "?destination=%s&domain=stellar.org&type=federation&user=%s" % (name, name)
+
+def get_forward_url(domain):
+
+	if domain in forward_url:
+		return forward_url[domain]
+	else:
+		get_federation_settings(domain)
+		if domain in forward_url:
+			return forward_url[domain]
+		else:
+			return None
+
+
+def get_federation_settings(domain):
+
+	global dirty
+	s = None
+
+	prefix = ['stellar.', 'www.', '']
+	for p in prefix:
+
+		url = 'https://%s%s/stellar.txt' % (p, domain)
+		req = urllib2.Request(url)
+
+		try:
+			response = urllib2.urlopen(req)
+		except urllib2.URLError:
+			pass
+		else:
+			s = response.read().splitlines()
+			break
+
+	while s:
+		if s[0] == '[federation_url]':
+			forward_url[domain] = s[1]
+			dirty = True
+			s = s[2:]
+		elif s[0] == '[reverse_federation_url]':
+			reverse_url[domain] = s[1]
+			dirty = True
+			s = s[2:]
+		else:
+			s = s[1:]
+
+#-------------------------------------------------------------------------------
+
+
+def get_accountid(name):
+
+	name = name.lower()
+	if not '@' in name:
+		name += '@stellar.org'
+
+	if name in account_id:
+		return account_id[name]
+
+	#
+
+	global dirty
+
+	name, domain = name.split('@')
+
+	url = get_forward_url(domain)
+	query = '?destination=%s&domain=%s&type=federation&user=%s' % (name, domain, name)
 
 	req = urllib2.Request(url + query)
 	response = urllib2.urlopen(req)
@@ -30,16 +90,29 @@ def get_accountid_from_username(name):
 	js = json.loads(res)
 
 	if 'federation_json' in js:
-		return js['federation_json']['destination_address']
+		res = js['federation_json']['destination_address']
+		account_id['%s@%s' % (name, domain)] = res
+		dirty = True
 	else:
-		return None
+		res = None
+
+	if dirty:
+		f = open(filename, 'wb')
+		pickle.dump(forward_url, f, -1)
+		pickle.dump(reverse_url, f, -1)
+		pickle.dump(account_id, f, -1)
+		pickle.dump(username, f, -1)
+		f.close()
+		dirty = False
+
+	return res
 
 #-------------------------------------------------------------------------------
 
 
-def get_username_from_accountid(account_id):
+def get_username(account_id):
 
-	url = REVERSE_FEDERATION_URL
+	url = reverse_url['stellar.org']
 	query = "?destination_address=%s&domain=stellar.org" % (account_id)
 
 	req = urllib2.Request(url + query)
@@ -53,3 +126,19 @@ def get_username_from_accountid(account_id):
 		return None
 
 #-------------------------------------------------------------------------------
+
+filename = 'federation.dat'
+dirty = False
+
+if os.path.exists(filename):
+	f = open(filename, 'rb')
+	forward_url = pickle.load(f)
+	reverse_url = pickle.load(f)
+	account_id  = pickle.load(f)
+	username    = pickle.load(f)
+	f.close()
+else:
+	get_federation_settings('stellar.org')
+
+#-------------------------------------------------------------------------------
+
