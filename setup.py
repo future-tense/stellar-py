55eca6071c326fdac9b68ba54eb6211f62ffaca2
Johan Stén <johan.sten@gmail.com>
2014-08-25 00:19:50 +0800

    Tabs

--- setup.py
+++ setup.py
@@ -12,24 +12,24 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 execfile('stellar/version.py')
 
 setup(
-    name='stellar-py',
-    version=__version__,
-    author='Johan Stén',
-    author_email='johan.sten@gmail.com',
-    packages=['stellar'],
-    scripts=['bin/stellar_keygen.py','bin/stellar_send.py'],
-    url='https://github.com/johansten/stellar-py',
-    license='LICENSE.txt',
-    description='Python client library for Stellar',
-    long_description=long_description,
-    classifiers =[
-        'Programming Language :: Python',
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules'],
-    install_requires=[
-        "simplejson", "ed25519"
-    ],
+	name='stellar-py',
+	version=__version__,
+	author='Johan Stén',
+	author_email='johan.sten@gmail.com',
+	packages=['stellar'],
+	scripts=['bin/stellar_keygen.py','bin/stellar_send.py'],
+	url='https://github.com/johansten/stellar-py',
+	license='LICENSE.txt',
+	description='Python client library for Stellar',
+	long_description=long_description,
+	classifiers =[
+		'Programming Language :: Python',
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Topic :: Software Development :: Libraries :: Python Modules'],
+	install_requires=[
+		"simplejson", "ed25519"
+	],
 )
