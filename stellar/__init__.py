3ded573088d22a6d51fad14c20a051ae7b7e4045
Johan Stén <johan.sten@gmail.com>
2014-08-25 00:18:59 +0800

    More refactoring for doing offline signing of transactions

--- stellar/__init__.py
+++ stellar/__init__.py
@@ -5,15 +5,10 @@ from transaction import *
 from federation import *
 
 
-def send_payment(secret, account, destination, amount):
+def get_payment_tx_json(account, destination, amount):
 
-	if not account:
-		seed	= Seed.from_human(secret)
-		account = AccountID.from_seed(seed).to_human()
-	else:
-		account = _translate_account_id(account)
-
-	amount = _parse_amount(amount)
+	account = _translate_account_id(account)
+	amount  = _parse_amount(amount)
 
 	tx_json = {
 		'TransactionType': 	'Payment',
@@ -27,6 +22,16 @@ def send_payment(secret, account, destination, amount):
 	tx_json['Destination'] = _translate_account_id(d[0])
 
 	complete_transaction_fields(tx_json)
+	return tx_json
+
+
+def send_payment(secret, account, destination, amount):
+
+	if not account:
+		seed	= Seed.from_human(secret)
+		account = AccountID.from_seed(seed).to_human()
+
+	tx_json = get_payment_tx_json(account, destination, amount)
 	tx_blob = sign_transaction(secret, tx_json)
 	submit_transaction(tx_blob)
 #	increase_sequence_number(account)
